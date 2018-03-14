08c08019d596df390d86b42abc5c4a58cc5d5fc7
Titch Deane <titch@hotmail.co.uk>
2018-03-13 17:14:04 -0700

    Implement 30 minute timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -13,40 +13,31 @@ class Timer extends Component {
     };
   }
 
-
-
   componentDidMount(){
-    // this.interval = setInterval(
-    //   () => {
-    //     if (this.state.timeRemaining > 0) {
-    //       this.setState(prevState => ({ timeRemaining: prevState.timeRemaining - 1 }))
-    //     } else {
-    //       clearInterval(this.interval);
-    //     }
-    //   }, 1000
-    // )
+
   }
 
   componentWillUnmount(){
     clearInterval(this.interval);
   }
 
-
-
   stopTimer(){
 
   }
 
-  startTimer(){
-    this.setState({ timeRemaining: this.state.timeRemaining })
+  startTimer(time){
+    if (this.state.completedSessions === 4) {
+      this.setState({ completedSessions: 0})
+    }
+    this.setState({ timeRemaining: time })
     clearInterval(this.interval);
     this.interval = setInterval(
       () => {
         if (this.state.timeRemaining > 0) {
           this.setState(prevState => ({ timeRemaining: prevState.timeRemaining - 1 }))
         } else {
-          this.setState({ timeRemaining: 300 })
-          this.setState({ onBreak: true })
+          this.state.completedSessions === 3 ? this.setState({ timeRemaining: 1800 }) : this.setState({ timeRemaining: 300 })
+          this.setState({ onBreak: true, completedSessions: this.state.completedSessions + 1 })
           clearInterval(this.interval);
         }
       }, 1000
@@ -54,27 +45,20 @@ class Timer extends Component {
   }
 
 
-
-
-  startBreakTimer(){
-    this.setState({ timeRemaining: this.state.breakTimeRemaining })
+  startBreakTimer(time){
+    this.setState({ timeRemaining: time })
     clearInterval(this.interval);
     this.interval = setInterval(
       () => {
         if (this.state.timeRemaining > 0) {
           this.setState(prevState => ({ timeRemaining: prevState.timeRemaining - 1 }))
         } else {
-          this.setState({ timeRemaining: 1500 })
-          this.setState({ onBreak: false })
+          this.setState({ timeRemaining: 1500, onBreak: false })
           clearInterval(this.interval);
         }
       }, 1000
     )
   }
-
-
-
-
 
 
   formatTime(timeInS){
@@ -89,20 +73,25 @@ class Timer extends Component {
     let newSession = null;
     let breakTime = null;
     if (this.state.timeRemaining < 1500 && this.state.onBreak === false) {
-        newSession = <div>Want to restart your existing session?
-                       <p><button onClick={ (e) => this.startTimer(e) }>Restart</button></p>
+        newSession = <div>Work Session
+                       <p><button onClick={ (e) => this.startTimer(1500) }>Restart Session</button></p>
                      </div>
     } else if (this.state.timeRemaining === 1500) {
         newSession = <div>Ready to start another session?
-                       <p><button onClick={ (e) => this.startTimer(e) }>Start</button></p>
+                       <p><button onClick={ (e) => this.startTimer(1500) }>Start Session</button></p>
                      </div>
     }
 
-    if (this.state.timeRemaining === 300) {
+  if (this.state.completedSessions % 4 === 0 && this.state.timeRemaining === 1800 && this.state.onBreak === true) {
+    breakTime = <div>Congratulations on completing 4 successive work sessions. Here, enjoy a 30 minute break to celebrate!
+                  <p><button onClick={ (e) => this.startBreakTimer(1800) }>Start Break</button></p>
+                </div>
+    } else if (this.state.timeRemaining === 2 && this.state.onBreak === true) {
       breakTime = <div>Ready for that well earned break?
-                    <p><button onClick={ (e) => this.startBreakTimer(e) }>Start Break</button></p>
+                    <p><button onClick={ (e) => this.startBreakTimer(300) }>Start Break</button></p>
                   </div>
     }
+
 
     return(
       <div>
@@ -110,6 +99,8 @@ class Timer extends Component {
 
         {breakTime}
         {newSession}
+        <p>Consecutive Completed Work Sessions: { this.state.completedSessions || 0 }</p>
+        <small>(Complete 4 in a row to earn a well-deserve 30 minute break!)</small>
 
       </div>
     );
